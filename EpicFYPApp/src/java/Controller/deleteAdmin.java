22dd114904eb5f9b825a89de0c642da239381f7f
Mediani Law <mediani.2015@sis.smu.edu.sg>
2018-10-04 22:50:14 +0800

    Added webservlet for deleteAdmin

--- EpicFYPApp/src/java/Controller/deleteAdmin.java
+++ EpicFYPApp/src/java/Controller/deleteAdmin.java
@@ -9,6 +9,7 @@ import Model.Dao.AdminDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Lenovo
  */
+@WebServlet(name = "deleteAdmin", urlPatterns = {"/deleteAdmin"})
 public class deleteAdmin extends HttpServlet {
 
     /**
